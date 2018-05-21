5d41b062f4a0cc61591cb4651389bab4d8059b3f
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-05-21 17:07:15 -0600

    require

--- keys.js
+++ keys.js
@@ -4,7 +4,7 @@ exports.twitter = {
   consumer_key: process.env.zCG8SdgvPN6HMTBPBNuiFVwNw,
   consumer_secret: process.env.KZaNPSH2U6WOXpN7293slbpqoufDFhEfp8qp9HgYed9LH9BWb2,
   access_token_key: process.env.998327152666804224-BdQ3KMMAGNspeUeIsGpDzWDZc38kFZa,
-  access_token_secret: process.env.	B7UNghbH2zOp3zm2y3lYLxINuquq0v2YLJr4nKm34yjTo
+  access_token_secret: process.env.B7UNghbH2zOp3zm2y3lYLxINuquq0v2YLJr4nKm34yjTo
 };
 
 exports.spotify = {
---
+++ liri.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: process.env.spotify_id,
+  secret: process.env.spotify_secret
+});
+ 
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
+
+
+
+
+
+
+
+
+
