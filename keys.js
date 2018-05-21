cd560fe5b5af27218ac21ef36f68ca3b87c2ce5c
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-05-20 19:58:27 -0600

    initial

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.zCG8SdgvPN6HMTBPBNuiFVwNw,
+  consumer_secret: process.env.KZaNPSH2U6WOXpN7293slbpqoufDFhEfp8qp9HgYed9LH9BWb2,
+  access_token_key: process.env.998327152666804224-BdQ3KMMAGNspeUeIsGpDzWDZc38kFZa,
+  access_token_secret: process.env.	B7UNghbH2zOp3zm2y3lYLxINuquq0v2YLJr4nKm34yjTo
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
